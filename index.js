32daec2d0cabee4b0267dcabe5d8b6f429787be4
Yiannis Stavrou <stav0049@gmail.com>
2019-12-02 18:43:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,40 +8,54 @@ function Trie(letter = '') {
 Trie.prototype = {
     add: function(word, node = this) {
         for (const letter of word) {
-            // Here we check if node.children[letter] already exists
-            // if it does we set each letter as a child node of its parent
+            // Here we check if node.children[letter] already exists (or check if the letter)
+            // already exists as a key.
+            // if it does we set the next letter as the child of the matching key.
             if (node.children[letter]) {
                 node = node.children[letter];
             } else {
                 // else the letters do not exist yet so we need to create
-                // a new parent node and trickle down
+                // a new node and trickle down
                 const newNode = new Trie(letter);
                 node.children[letter] = newNode;
-                node = newNode;
+                node = newNode;// node keeps getting reassigned to each letter. Ending
+                // in the last letter of the word.
             }
         }
-      // after letter nodes are added we set isWord to true
-      // keep in mind that node.children is our current node
+      // After letter nodes are added we set isWord to true on the LAST
+      // letter (node). Keep in mind that node keeps getting reassigned to the next letter
+      // until we are finished looping. So the word `shark` would have `k` finally set to
+      // .isWord = true since it is the last letter of our word.
       node.isWord = true;
     },
     find: function(word, node = this) {
         let value = ''
         for (const letter of word) {
+            // check if letter already exists as a key
             if (node.children[letter]) {
+                // and assign node to each letter of the word as a child of the previous.
                 node = node.children[letter];
+                // value concatenates to form the entire word. so `a` + `p` + `p` + `l` + `e`
+                // will give us apple as the final `value`.
                 value += letter;
             }
         }
+        // finally we check if value equals the word we passed in
+        // if it does we return the final node (or final letter). So with `apple`
+        // we would return the node `e`.
         return value === word ? node : null;
     },
     remove: function(word = '', node = this) {
+        // if word does not exist - we exit with a return statement
         if (!word) {
             return null;
         }
         const chain = [];
+        // chain will be used to store the letters of our word.
   
         // traverse down trie
         for (const letter of word) {
+            // if letter is found as a `key` in node.children...
             if (node.children[letter]) {
                 chain.push(node) // we want all nodes accessible in chain so we can move backwards and remove dangling nodes
                 node = node.children[letter]
@@ -49,7 +63,7 @@ Trie.prototype = {
                 return null; // word is not in trie
             }
         }
-  
+        
         if (Object.keys(node.children).length) { // if any children, we should only change isWord flag
             node.isWord = false;
             return node;
@@ -57,10 +71,20 @@ Trie.prototype = {
   
         // Zero children in node.
         // continue until we hit a breaking condition
+        
+        // if chain has length we set child to the last node and pop it off.
         let child = chain.length ? chain.pop() : null; // whatever node was
+        
+        // if chain has length we set parent to the last node and pop it off.
         let parent = chain.length ? chain.pop() : null; // if node has parent
         while (true) {
         child && parent && delete parent.children[child.value]; // remove child;
+        // short circuit evaluation. Essentiall equivalent to:
+        /*
+            if (child && parent) {
+                delete parent.children[child.value]
+            }
+        */
 
         if (Object.keys(parent.children).length || !chain.length) { // if more children or chain is empty, we're done!
             node.isWord = false;
